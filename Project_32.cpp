de1b78711092b1849cf13446139b313798ec22e0
Baskerville <luillixd@outlook.es>
2023-11-27 10:58:53 -0600

    Coreccion de problemas de apartados

--- Project_32.cpp
+++ Project_32.cpp
@@ -3,81 +3,114 @@
 #include <sys/sem.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define READCOUNT_KEY 1234
 #define SEM_KEY 5678
 
 void reader(int semid, int* readcount) {
+    struct sembuf op;
     while(1) {
-        // Wait on semaphore
-        struct sembuf op = {0, -1, 0};
-        semop(semid, &op, 1);
+        // Error checking for semop
+        op.sem_num = 0; op.sem_op = -1; op.sem_flg = 0;
+        if (semop(semid, &op, 1) == -1) {
+            perror("semop");
+            exit(1);
+        }
 
         (*readcount)++;
 
         if (*readcount == 1) {
-            // First reader locks the resource for writers
             op.sem_num = 1;
-            semop(semid, &op, 1);
+            if (semop(semid, &op, 1) == -1) {
+                perror("semop");
+                exit(1);
+            }
         }
 
-        // Release semaphore
-        op.sem_num = 0;
-        op.sem_op = 1;
-        semop(semid, &op, 1);
+        op.sem_num = 0; op.sem_op = 1; op.sem_flg = 0;
+        if (semop(semid, &op, 1) == -1) {
+            perror("semop");
+            exit(1);
+        }
 
-        // Read
         printf("Reading\n");
 
-        // Wait on semaphore
-        op.sem_num = 0;
-        op.sem_op = -1;
-        semop(semid, &op, 1);
+        op.sem_num = 0; op.sem_op = -1; op.sem_flg = 0;
+        if (semop(semid, &op, 1) == -1) {
+            perror("semop");
+            exit(1);
+        }
 
         (*readcount)--;
 
         if (*readcount == 0) {
-            // Last reader unlocks the resource for writers
-            op.sem_num = 1;
-            op.sem_op = 1;
-            semop(semid, &op, 1);
+            op.sem_num = 1; op.sem_op = 1; op.sem_flg = 0;
+            if (semop(semid, &op, 1) == -1) {
+                perror("semop");
+                exit(1);
+            }
         }
 
-        // Release semaphore
-        op.sem_num = 0;
-        op.sem_op = 1;
-        semop(semid, &op, 1);
+        op.sem_num = 0; op.sem_op = 1; op.sem_flg = 0;
+        if (semop(semid, &op, 1) == -1) {
+            perror("semop");
+            exit(1);
+        }
     }
 }
 
 void writer(int semid) {
+    struct sembuf op;
     while(1) {
-        // Wait on semaphore
-        struct sembuf op = {1, -1, 0};
-        semop(semid, &op, 1);
+        op.sem_num = 1; op.sem_op = -1; op.sem_flg = 0;
+        if (semop(semid, &op, 1) == -1) {
+            perror("semop");
+            exit(1);
+        }
 
-        // Write
         printf("Writing\n");
 
-        // Release semaphore
-        op.sem_op = 1;
-        semop(semid, &op, 1);
+        op.sem_num = 1; op.sem_op = 1; op.sem_flg = 0;
+        if (semop(semid, &op, 1) == -1) {
+            perror("semop");
+            exit(1);
+        }
     }
 }
 
 int main() {
-    // Create shared memory for readcount
     int shmid = shmget(READCOUNT_KEY, sizeof(int), 0666 | IPC_CREAT);
+    if (shmid == -1) {
+        perror("shmget");
+        exit(1);
+    }
+
     int* readcount = (int*) shmat(shmid, 0, 0);
+    if (readcount == (void *) -1) {
+        perror("shmat");
+        exit(1);
+    }
     *readcount = 0;
 
-    // Create semaphores
     int semid = semget(SEM_KEY, 2, 0666 | IPC_CREAT);
-    semctl(semid, 0, SETVAL, 1); // x
-    semctl(semid, 1, SETVAL, 1); // wsem
+    if (semid == -1) {
+        perror("semget");
+        exit(1);
+    }
 
-    // Create reader and writer processes
-    if (fork() == 0) {
+    if (semctl(semid, 0, SETVAL, 1) == -1 || semctl(semid, 1, SETVAL, 1) == -1) {
+        perror("semctl");
+        exit(1);
+    }
+
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        exit(1);
+    }
+
+    if (pid == 0) {
         reader(semid, readcount);
     } else {
         writer(semid);
