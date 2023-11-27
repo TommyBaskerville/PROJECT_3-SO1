e3989886831b3f38ef7b304956f0ffaa73844871
Baskerville <luillixd@outlook.es>
2023-11-27 14:09:04 -0600

    Verison Final de Lectores y escritores

--- Project_32.cpp
+++ Project_32.cpp
@@ -5,50 +5,27 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <semaphore.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 
-#define SHM_KEY 1234
+#define FILE_PATH "sharedfile.txt"
 
 struct rw_semaphore {
     sem_t mutex;
     sem_t write_lock;
-    int *read_count;
 };
 
 void init_rwsem(struct rw_semaphore *rwsem) {
-    sem_init(&rwsem->mutex, 1, 1); // Inicializado con 1 para evitar race conditions en la creación de la memoria compartida
-    sem_init(&rwsem->write_lock, 1, 1); // Inicializado con 1 para evitar race conditions en la creación de la memoria compartida
-
-    int shm_id = shmget(SHM_KEY, sizeof(int), IPC_CREAT | 0666);
-    if (shm_id == -1) {
-        perror("shmget");
-        exit(EXIT_FAILURE);
-    }
-
-    rwsem->read_count = static_cast<int*>(shmat(shm_id, NULL, 0));
-    if (rwsem->read_count == (int *)-1) {
-        perror("shmat");
-        exit(EXIT_FAILURE);
-    }
-
-    *(rwsem->read_count) = 0;
+    sem_init(&rwsem->mutex, 1, 1); // Mutex para control de acceso a la sección crítica
+    sem_init(&rwsem->write_lock, 1, 1); // Semáforo para bloquear escritura
 }
 
 void down_read(struct rw_semaphore *rwsem) {
     sem_wait(&rwsem->mutex);
-    (*(rwsem->read_count))++;
-    if (*(rwsem->read_count) == 1) {
-        sem_wait(&rwsem->write_lock);
-    }
-    sem_post(&rwsem->mutex);
 }
 
 void up_read(struct rw_semaphore *rwsem) {
-    sem_wait(&rwsem->mutex);
-    (*(rwsem->read_count))--;
-    if (*(rwsem->read_count) == 0) {
-        sem_post(&rwsem->write_lock);
-    }
     sem_post(&rwsem->mutex);
 }
 
@@ -60,21 +37,48 @@ void up_write(struct rw_semaphore *rwsem) {
     sem_post(&rwsem->write_lock);
 }
 
-void reader(struct rw_semaphore *rwsem) {
+void reader(struct rw_semaphore *rwsem, int id) {
+    FILE *file;
+    char buffer[256];
+
     while (1) {
         down_read(rwsem);
-        // Perform reading
-        printf("Reader reading... Read count: %d\n", *(rwsem->read_count));
+
+        file = fopen(FILE_PATH, "r");
+        if (file == NULL) {
+            perror("Error opening file");
+            exit(EXIT_FAILURE);
+        }
+
+        // Leer del archivo
+        while (fgets(buffer, sizeof(buffer), file) != NULL) {
+            printf("Reader %d read: %s", id, buffer);
+        }
+
+        fclose(file);
+
         up_read(rwsem);
         sleep(1);
     }
 }
 
-void writer(struct rw_semaphore *rwsem) {
+void writer(struct rw_semaphore *rwsem, int id) {
+    FILE *file;
+
     while (1) {
         down_write(rwsem);
-        // Perform writing
-        printf("Writer writing...\n");
+
+        file = fopen(FILE_PATH, "a");
+        if (file == NULL) {
+            perror("Error opening file");
+            exit(EXIT_FAILURE);
+        }
+
+        // Escribir en el archivo
+        fprintf(file, "Writer %d wrote.\n", id);
+
+        fclose(file);
+
         up_write(rwsem);
         sleep(1);
     }
@@ -85,40 +89,37 @@ int main() {
     init_rwsem(&rwsem);
 
     pid_t pid;
+    int reader_count = 3;
+    int writer_count = 2;
 
-    // Create reader processes
-    for (int i = 0; i < 3; i++) {
+    // Crear procesos lectores
+    for (int i = 0; i < reader_count; i++) {
         pid = fork();
         if (pid == -1) {
             perror("fork");
             exit(EXIT_FAILURE);
-        } else if (pid == 0) { // Child process
-            reader(&rwsem);
+        } else if (pid == 0) { // Proceso hijo
+            reader(&rwsem, i + 1);
             exit(EXIT_SUCCESS);
         }
     }
 
-    // Create writer processes
-    for (int i = 0; i < 2; i++) {
+    // Crear procesos escritores
+    for (int i = 0; i < writer_count; i++) {
         pid = fork();
         if (pid == -1) {
             perror("fork");
             exit(EXIT_FAILURE);
-        } else if (pid == 0) { // Child process
-            writer(&rwsem);
+        } else if (pid == 0) { // Proceso hijo
+            writer(&rwsem, i + 1);
             exit(EXIT_SUCCESS);
         }
     }
 
-    // Wait for child processes to finish
-
-    for (int i = 0; i < 5; i++) {
+    // Esperar a que los procesos hijos terminen
+    for (int i = 0; i < reader_count + writer_count; i++) {
         wait(NULL);
     }
-
-    // Detach and remove shared memory
-    shmdt(rwsem.read_count);
-    shmctl(shmget(SHM_KEY, sizeof(int), 0), IPC_RMID, NULL);
 
     return 0;
 }
